da9a344e7394ee436cf701a1cbc86269948f9f01
amurunoghenetejiri <amurunoghenetejiri@gmail.com>
2026-07-28 22:27:36 +0100

    Update Layout.tsx

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -9,7 +9,7 @@ export default function Layout() {
     <div className="min-h-screen flex flex-col">
       <Navbar />
 
-      <main className="flex-1 pt-24 animate-fade-in">
+      <main className="flex-1 pt-16 sm:pt-20 md:pt-24 animate-fade-in">
         <Outlet />
       </main>
 
