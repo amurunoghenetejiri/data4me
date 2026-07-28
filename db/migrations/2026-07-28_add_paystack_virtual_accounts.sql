14c1e23f206e432c13bb1a0d0d290f53a14e9bee
amurunoghenetejiri <amurunoghenetejiri@gmail.com>
2026-07-28 13:45:48 +0100

    Update 2026-07-28_add_paystack_virtual_accounts.sql

--- db/migrations/2026-07-28_add_paystack_virtual_accounts.sql
+++ db/migrations/2026-07-28_add_paystack_virtual_accounts.sql
@@ -6,4 +6,6 @@ ADD COLUMN IF NOT EXISTS account_name TEXT,
 ADD COLUMN IF NOT EXISTS account_number TEXT,
 ADD COLUMN IF NOT EXISTS customer_code TEXT,
 ADD COLUMN IF NOT EXISTS dedicated_account_id TEXT,
-ADD COLUMN IF NOT EXISTS dedicated_account_assigned BOOLEAN DEFAULT FALSE;
+ADD COLUMN IF NOT EXISTS dedicated_account_assigned BOOLEAN DEFAULT FALSE,
+ADD COLUMN IF NOT EXISTS dedicated_account_number TEXT,
+ADD COLUMN IF NOT EXISTS dedicated_bank_name TEXT;
